9625aab681157f6b865c3e515b52e440c8215c7f
HerMat321 <herdzina.mateuszdev@gmail.com>
2025-01-21 14:24:42 +0100

    Exam Completed 21.01.2025

---
+++ Czerwiec_2023_zadanie_6_PHP/database.php
@@ -0,0 +1,9 @@
+<?php
+    #Skrypt połączenia z bazą danych
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "biblioteka";
+
+    $connection = new mysqli($host,$user,$password,$database);
+?>
